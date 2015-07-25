376fd5ae40c2bf35c03e758ddc7e690c74098351
Bob Yexley <bob@yexley.net>
2015-07-24 21:36:01 -0500

    Version bumped to 0.2.1

--- dist/messenger.js
+++ dist/messenger.js
@@ -1,6 +1,6 @@
-// Cedar, v0.2.0 | (c) 2015 Bob Yexley
+// Cedar, v0.2.1 | (c) 2015 Bob Yexley
 // Description: A mixin for adding postal.js messaging functionality to your modules
-// Built: July 24, 2015 21:21:47.27
+// Built: July 24, 2015 21:32:56.976
 // Homepage: https://github.com/ryexley/sumac
 // License: MIT (http://www.opensource.org/licenses/mit-license)
 
