f2633d9ee73f773384ef25af8db2aa2435b21dda
Bob Yexley <bob@yexley.net>
2015-07-26 10:32:17 -0500

    v0.2.2

--- dist/messenger.js
+++ dist/messenger.js
@@ -1,6 +1,6 @@
-// Sumac, v0.2.2 | (c) 2015 Bob Yexley
+// sumac, v0.2.2 | (c) 2015 Bob Yexley
 // Description: A mixin for adding postal.js messaging functionality to your modules
-// Built: July 24, 2015 21:41:25.529
+// Built: July 26, 2015 10:32:8.840
 // Homepage: https://github.com/ryexley/sumac
 // License: MIT (http://www.opensource.org/licenses/mit-license)
 
